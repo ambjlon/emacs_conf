3f798f4a3b757ad6b15a358582bb52bbc65bb34e
杨龙 <jianglong.cjl@alibaba-inc.com>
2015-08-17 16:33:06 +0800

    add elap browse-kill-ring -+

---
+++ elpa/browse-kill-ring+-20150104.2132/browse-kill-ring+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "browse-kill-ring+" "20150104.2132" "Extensions to `browse-kill-ring.el'." '((browse-kill-ring "0")) :url "http://www.emacswiki.org/browse-kill-ring+.el" :keywords '("convenience"))
