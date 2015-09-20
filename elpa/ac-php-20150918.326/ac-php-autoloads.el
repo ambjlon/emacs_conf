5606c3f531a53a08a0aa9c36af9bee25d83f6766
xiaoju <xiaoju@bf188f3836fc.(none)>
2015-09-20 10:45:10 +0800

    for php up in xiaoju vm

---
+++ elpa/ac-php-20150918.326/ac-php-autoloads.el
@@ -0,0 +1,17 @@
+;;; ac-php-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ac-php-comm-tags-data.el" "ac-php-pkg.el"
+;;;;;;  "ac-php-remake-tags-bat.el" "ac-php-remake-tags-without-cscope-bat.el"
+;;;;;;  "ac-php.el") (22014 6212 810697 764000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-php-autoloads.el ends here
