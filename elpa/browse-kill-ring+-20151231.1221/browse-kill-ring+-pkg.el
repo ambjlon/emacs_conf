4ab438a349cc68a24b9caec6bda3e5533602910c
chenjianglong <chenjianglong@didichuxing.com>
2016-09-12 04:23:19 -0400

    sdfas

---
+++ elpa/browse-kill-ring+-20151231.1221/browse-kill-ring+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "browse-kill-ring+" "20151231.1221" "Extensions to `browse-kill-ring.el'." '((browse-kill-ring "0")) :url "http://www.emacswiki.org/browse-kill-ring+.el" :keywords '("convenience"))
