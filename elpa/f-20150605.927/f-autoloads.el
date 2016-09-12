2b48edce1b862e2976b464280f2803e98c292f93
chenjianglong <chenjianglong@didichuxing.com>
2016-09-12 04:31:25 -0400

    update 30 packages

--- elpa/f-20150605.927/f-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("f.el") (22009 45079 494316 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; f-autoloads.el ends here
